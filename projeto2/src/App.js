7e156ae9c6e0e3a3386707e0b86da97e7d349b0c
fernandocfbf <fernandocfbf@al.insper.edu.br>
2020-10-27 08:49:45 -0300

    Corrigi os erros do projeto

--- projeto2/src/App.js
+++ projeto2/src/App.js
@@ -15,30 +15,6 @@ function App() {
     );
 }
 
-function cadastrar() {
-    axios.post('http://localhost:3003/usuarios', this.state.usuario).then(resp => {
-        if (Math.floor(resp.status / 100) === 2) {
-            this.setState((state) => {
-                return {
-                    lista: [...state.lista, state.usuario],
-                    usuario: { username: '' }
-                }
-            })
-            return;
-        }
-        console.log(resp)
-    })
-        .catch(erro => console.log(erro))
-}
 
-// event.target representa o elemento que causou o trigger do evento que chamou o método (no caso, será o input do nome)
-function handleChange(event) {
-    var handleState = (state, event) => {
-        state.usuario[event.target.name] = event.target.value
-        return state
-    }
-
-    this.setState(handleState(this.state, event))
-}
 
 export default App;
--- projeto2/src/usuarios.jsx
+++ projeto2/src/usuarios.jsx
@@ -26,6 +26,32 @@ export default class Usuarios extends Component {
         this.cadastrar = this.cadastrar.bind(this)
     }
 
+    cadastrar() {
+        axios.post('http://localhost:3000/adduser', this.state.usuario).then(resp => {
+            if (Math.floor(resp.status / 100) === 2) {
+                this.setState((state) => {
+                    return {
+                        lista: [...state.lista, state.usuario],
+                        usuario: { username: '' }
+                    }
+                })
+                return;
+            }
+            console.log(resp)
+        })
+            .catch(erro => console.log(erro))
+    }
+
+    // event.target representa o elemento que causou o trigger do evento que chamou o método (no caso, será o input do nome)
+    handleChange(event) {
+        var handleState = (state, event) => {
+            state.usuario[event.target.name] = event.target.value
+            return state
+        }
+
+        this.setState(handleState(this.state, event))
+    }
+
     render() {
         var usuarios = this.state.lista
         var liUsuarios = usuarios.map(usuario => {
@@ -50,3 +76,4 @@ export default class Usuarios extends Component {
             </div>
         )
     }
+}
