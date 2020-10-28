713d25677768f782b130226db7434f0193325b25
fernandocfbf <fernandocfbf@al.insper.edu.br>
2020-10-27 22:59:02 -0300

    Os valores de username e senha chegam no back

--- projeto2/src/login.jsx
+++ projeto2/src/login.jsx
@@ -8,47 +8,34 @@ export default class Login extends Component {
         // Inicializando o State com alguns valores para testarmos
         //é necessario iniciar o state
         this.state = {
-            success: false
+            usuario: [
+                { nome: '' },
+                { username: 'fernandocfbf' },
+                { senha: '123' },
+                { erro: '' }
+            ]
         }
 
         this.handleChange = this.handleChange.bind(this)
-        this.cadastrar = this.cadastrar.bind(this)
+        //this.cadastrar = this.cadastrar.bind(this)
         this.entrar = this.entrar.bind(this)
     }
 
-    
-    entrar(){
+
+    entrar() {
         // Fazendo a requisição assíncrona do GET lista de usuários e atualizando o state
-        axios.post('http://localhost:3000/login')
+        axios.post('http://localhost:3000/login', {username: this.state.usuario.username, senha: this.state.usuario.senha})
             .then(resp => {
-                console.log(resp)
+                console.log(resp.data)
                 if (Math.floor(resp.status / 100) === 2) { // Checa se o response status code é 2XX(sucesso)
-                    this.setState({ success: resp })
+                    this.setState({ usuario: resp })
+
+
                     return;
                 }
-                console.log(resp)
             })
             .catch(erro => console.log(erro))
     }
-
-    cadastrar() {
-        axios.post('http://localhost:3000/adduser', this.state.usuario).then(resp => {
-            if (Math.floor(resp.status / 100) === 2) {
-                this.setState((state) => {
-                    return {
-                        lista: [...state.lista, state.usuario],
-                        usuario: { username: '' }
-                    }
-                })
-                return;
-            }
-            console.log(resp)
-        })
-            .catch(erro => console.log(erro))
-    }
-
-
-    
 
     // event.target representa o elemento que causou o trigger do evento que chamou o método (no caso, será o input do nome)
     handleChange(event) {
@@ -61,32 +48,19 @@ export default class Login extends Component {
     }
 
     render() {
-
-        //usuarios é uma lista do state
-        var usuarios = this.state.lista
-
-        //recebe func -> for -> exect/porElemento
-        var liUsuarios = usuarios.map(usuario => {
-            return (
-                <li key={usuario.username}>{usuario.username}</li>
-            )
-        })
         return (
             <div>
 
-                <label>Username</label>
+                <label>Username: </label>
                 <input name="username"
                     value={this.state.usuario.username}
-                    onChange={this.handleChange} />
-                <label>Senha</label>
-                
+                    onChange={this.handleChange} /><br></br>
+                <label>Senha: </label>
                 <input name="senha"
                     value={this.state.usuario.senha}
-                    onChange={this.handleChange}/>
+                    onChange={this.handleChange} /><br></br>
 
                 <button onClick={this.entrar}>Entrar</button>
-
-
             </div>
         )
     }
